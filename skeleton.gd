8a84a2b18c5426f1047977a74654cb99d5abca81
ejohansen26 <112412176+ejohansen26@users.noreply.github.com>
2024-05-28 03:17:27 -0400

    Populated Area

--- skeleton.gd
+++ skeleton.gd
@@ -4,6 +4,7 @@ signal hit
 signal dead
 
 const SPEED = 5.0
+var is_active = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -11,3 +12,6 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	move_and_slide()
+
+func _on_visible_on_screen_notifier_screen_entered():
+	is_active = true
